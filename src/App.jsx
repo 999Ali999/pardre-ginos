fc1cd22f8595ccf855577b200f736dee63d815e6
999Ali999 <999alishafiei999@gmail.com>
2025-01-04 12:42:35 +0100

    replace Pizza component with Order component; implement pizza ordering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Pepperoni"
-        description="A classic pizza with pepperoni slices."
-        image={"public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="A controversial pizza with ham and pineapple."
-        image={"public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Americano"
-        description="french fries, hamburger, and cheese"
-        image={"public/pizzas/big_meat.webp"}
-      />
+      <h1>Padre Gino's Pizza</h1>
+      <Order />
     </div>
   );
 };
